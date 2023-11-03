badc219184dce4bb96b159b01b36f81893e4d055
Jacek Nogaj <jacek.nogaj@gmail.com>
2023-11-03 15:18:27 +0100

    corrected loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 const currencyForm = document.getElementById("currency-form");
 const resultElement = document.getElementById("result");
-const loader = document.getElementById("loader");
 
 currencyForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  loader.style.display = "block";
   const formData = new FormData(currencyForm);
   const selectedCurrency = formData.get("currency");
   const amount = parseFloat(formData.get("amount"));
@@ -12,23 +10,36 @@ currencyForm.addEventListener("submit", function (event) {
   if (isNaN(amount) || amount <= 0) {
     resultElement.innerHTML =
       '<span class="error">Wprowadź prawidłową kwotę, większą od 0</span>';
-    loader.style.display = "none";
     return;
   }
+
+  const loader = document.createElement("div");
+  loader.id = "loader";
+  loader.innerText = "Pobieranie kursu...";
+  resultElement.innerHTML = "";
+  resultElement.appendChild(loader);
 
   fetch(
     `https://api.nbp.pl/api/exchangerates/rates/a/${selectedCurrency}/?format=json`
   )
     .then((response) => response.json())
     .then((data) => {
-      const rate = data.rates[0].mid;
-      const convertedAmount = (amount * rate).toFixed(2);
-      resultElement.innerText = `${amount} ${selectedCurrency} = ${convertedAmount} PLN`;
-      loader.style.display = "none";
+      const rate = data.rates?.[0]?.mid;
+
+      if (rate) {
+        const convertedAmount = (amount * rate).toFixed(2);
+        resultElement.innerText = `${amount} ${selectedCurrency} = ${convertedAmount} PLN`;
+      } else {
+        resultElement.innerText = "Brak danych kursu dla wybranej waluty.";
+      }
     })
     .catch((error) => {
       resultElement.innerText =
         "Wystąpił błąd podczas pobierania kursu waluty.";
-      loader.style.display = "none";
+    })
+    .finally(() => {
+      if (loader) {
+        loader.style.display = "none"; // Wyłączanie loadera niezależnie od wyniku zapytania.
+      }
     });
 });
